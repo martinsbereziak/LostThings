7a399385653dd2d598527a9a94cc5b7882dc2a8a
martinsbereziak <bereziakmarta@gmail.com>
2023-05-03 10:45:12 +0300

    first commit

---
+++ src/main/java/com/example/buysell/LostThingsApplication.java
@@ -0,0 +1,11 @@
+package com.example.buysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostThingsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LostThingsApplication.class, args);
+	}
+}
